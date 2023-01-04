fdd0b1d3bedfde2e733571da610cec60c78adc48
t0in4 <t0in4@yahoo.com>
2023-01-04 14:39:24 +0300

    Дописать код

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int start = // дописать код;
+        int end = // дописать код;
+        for (int i = start; i <= end; i++) {
+            if (<?>) {
+                System.out.println("FizzBuzz");
+                } else if (<?>) {
+                System.out.println("Fizz");
+            } else if (<?>) {
+                
+                System.out.println("Buzz");
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
